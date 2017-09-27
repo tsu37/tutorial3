ba7a5e1824499374a88d964113e68ebd9349b880
tsutomu37 <bull.bull.bull91@gmail.com>
2017-09-27 11:22:13 +0000

    list11.24

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,7 +11,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
-end
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
 
   test "valid signup information" do
     get signup_path
@@ -22,7 +24,7 @@ end
                                          password_confirmation: "password" } }
     end
     follow_redirect!
-    assert_template 'users/show'
-    assert is_logged_in?
+    # assert_template 'users/show'
+    # assert is_logged_in?
   end
 end
