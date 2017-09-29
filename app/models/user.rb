a2c6c109571279fdc028ecc8bf4e3b83e667ec9f
tsutomu37 <bull.bull.bull91@gmail.com>
2017-09-29 16:33:09 +0000

    list_14.44

--- app/models/user.rb
+++ app/models/user.rb
@@ -114,6 +114,21 @@ class User < ApplicationRecord
   def following?(other_user)
     following.include?(other_user)
   end
+  
+    # パスワード再設定の期限が切れている場合はtrueを返す
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
+  # ユーザーのステータスフィードを返す
+  def feed
+    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+  end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
 
   private
 
